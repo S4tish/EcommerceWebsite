11aa7e164606804a7a073c46e2f063ebebbcfc27
satish singh <101500124+S4tish@users.noreply.github.com>
2023-05-01 08:01:59 -0400

    Add files via upload

---
+++ learn-react/src/App.js
@@ -0,0 +1,22 @@
+
+
+import React from 'react';
+import Navbar from './Navbar.js';
+import Product from './Product.js';
+import Form from './Form.js';
+ import Footer from './Footer.js';
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Product/>
+      <Form />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
+
---
+++ learn-react/src/Product.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import './Product.css';
+
+const Product = () => {
+  return (
+    <>
+    <h1>PRODUCTS</h1>
+<div class="grid grid-cols-2 md:grid-cols-3 gap-7">
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" alt="" />
+        <p>Oil</p>
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt="" />
+        <p>Shoes</p>
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt=""/>
+        <p>Carry Bag</p>
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt=""/>
+        <p>Home Decoration</p>
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" alt=""/>
+        <p>Watch</p>
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg" alt=""/>
+        <p>Sport Shoes</p>
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg" alt=""/>
+        <p>Cleanser</p>
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg" alt=""/>
+        <p>Carry Bag</p>
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg" alt=""/>
+        <p>Decoration Bag</p>
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg" alt=""/>
+        <p>Purfume</p>
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg" alt=""/>
+        <p>Games</p>
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg" alt=""/>
+        <p>Bag</p>
+    </div>
+</div>
+
+    </>
+  )
+}
+
+export default Product
